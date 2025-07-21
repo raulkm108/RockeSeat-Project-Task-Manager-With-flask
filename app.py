1ae2411fdbb8eacd7b42f09e9d893d8381d29051
raulkm108 <raulkm.civil@gmail.com>
2025-07-21 10:45:40 -0300

    Added global
    Addes global task_id_control to call it inside the method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models.task import Task
 
 app = Flask(__name__)
@@ -8,12 +8,12 @@ task_id_control = 1
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
+    global task_id_control
     data = request.get_json()
     new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
     task_id_control += 1
     tasks.append(new_task)
     print(tasks)
-    return 'test'
-
+    return jsonify({"message": "New task created successfully"})
 if __name__ == "__main__":
     app.run(debug=True)
