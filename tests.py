9fea653af081452a8b463efe5f6660a089b21ea4
raulkm108 <raulkm.civil@gmail.com>
2025-07-23 11:46:26 -0300

    Created an automated tester for creating task

---
+++ tests.py
@@ -0,0 +1,13 @@
+import requests
+import pytest
+
+BASE_URL = 'http://127.0.0.1:5000'
+tasks = []
+
+def test_create_task():
+    new_task_data = {
+        "title": "New task",
+        "description": "Descripition of new task"
+    }
+    response = requests.post(f"{BASE_URL}/tasks", json=new_task_data)
+    assert response.status_code == 200
