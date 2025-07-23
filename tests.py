50ccd019b78eea6d1b5a48a254addd9fc503f6b5
raulkm108 <raulkm.civil@gmail.com>
2025-07-23 12:31:50 -0300

    Creating a test for specific tasks

--- tests.py
+++ tests.py
@@ -22,3 +22,9 @@ def test_get_tasks():
     response_json = response.json()
     assert "tasks" in response_json
     assert "total_tasks" in response_json
+
+def test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
